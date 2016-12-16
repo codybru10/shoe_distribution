0608b1a5d84b9d8249f877f2de5b1d848b2a6c3e
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-16 09:58:53 -0800

    add ability to view list of brands and stores

--- app.rb
+++ app.rb
@@ -6,3 +6,31 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/stores') do
+  @stores = Store.all
+  erb(:stores)
+end
+
+post('/add_store') do
+  store_name = params[:store_name]
+  @store = Store.create({:name => store_name})
+  @stores = Store.all
+  redirect('/stores')
+end
+
+get('/brand/:id') do
+  erb(:brand)
+end
+
+get('/brands') do
+  @brands = Brand.all
+  erb(:brands)
+end
+
+post('/add_brand') do
+  brand_name = params[:brand_name]
+  @brand = Brand.create({:name => brand_name})
+  @brands = Brand.all
+  redirect('/brands')
+end
