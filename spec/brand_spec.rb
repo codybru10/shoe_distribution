bfcbe74edf214bf394a2487bf2d0c0a404532469
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-16 09:11:09 -0800

    adds validation for brands and stores

---
+++ spec/brand_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe(Brand) do
+  it('adds a shoe brand to a store') do
+    test_store = Store.create({:name => 'Foot Locker'})
+    test_brand = Brand.create({:name => 'Nike'})
+    test_brand.stores.push(test_store)
+    expect(test_brand.stores()).to(eq([test_store]))
+  end
+
+  it('validates presence for the name of the brand') do
+    test_brand = Brand.new({:name => ""})
+    expect(test_brand.save()).to(eq(false))
+  end
+
+
+end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe(Store) do
-    it('adds a shoe brand to a store') do
-      test_store = Store.create({:name => 'Foot Locker'})
-      test_brand = Brand.create({:name => 'Nike'})
-      test_store.brands.push(test_brand)
-      expect(test_store.brands()).to(eq([test_brand]))
+  it('adds a shoe brand to a store') do
+    test_store = Store.create({:name => 'Foot Locker'})
+    test_brand = Brand.create({:name => 'Nike'})
+    test_store.brands.push(test_brand)
+    expect(test_store.brands()).to(eq([test_brand]))
   end
+
+  it('validates presence for the name of the store') do
+    test_store = Store.new({:name => ""})
+    expect(test_store.save()).to(eq(false))
+  end
+
 end
